12b452a67f9a15cd6169f35fc203926a651f7b03
Joshua Dietzenbach <jmdietz12@gmail.com>
2025-04-21 16:24:56 -0400

    built template, in the middle of fixing

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studyProject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studySpot.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
