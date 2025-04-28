1ed4644a2a21d8364f44e1d66d05f3c3c8d80484
SubhanAamir <Subhan.aamir96@gmail.com>
2025-04-28 00:22:05 -0400

    Fixed Event page

--- events/forms.py
+++ events/forms.py
@@ -4,4 +4,4 @@ from .models import StudyEvent
 class StudyEventForm(forms.ModelForm):
     class Meta:
         model = StudyEvent
-        fields = ['title', 'description', 'location', 'time']
+        fields = ['title', 'location', 'time', 'spots', 'address']
