ab9bd6b30244e44723a97a558453ce794bd4f536
Student <jcampbell350@gatech.edu>
2025-04-29 00:43:38 -0400

    save account info

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+#automatically create a profile when a user is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from user_profile import views
 
 urlpatterns = [
-    path('profile/', views.profile, name='profile'),
+    path('profile/', views.profile_view, name='profile'),
 ]
