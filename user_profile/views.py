c52bca55394e80e1685e79a245b69fb98858a8cf
Student <jcampbell350@gatech.edu>
2025-04-29 01:55:43 -0400

    upcoming and past joined events

--- user_profile/views.py
+++ user_profile/views.py
@@ -4,18 +4,26 @@ from django.contrib import messages
 from .models import Profile
 from django.contrib.auth.models import User
 from events.models import StudyEvent
+from django.utils import timezone
 
 
 @login_required
 def profile_view(request):
     profile, created = Profile.objects.get_or_create(user=request.user)
-
+    time = timezone.now()
     #get study events created by the user
     created_events = StudyEvent.objects.filter(created_by=request.user).order_by('-time')
 
-    #get study events that the user has joined
-    joined_events = StudyEvent.objects.filter(participants=request.user).exclude(created_by=request.user).order_by(
-        '-time')
+    #user story 12
+    upcoming_events = StudyEvent.objects.filter(participants=request.user,
+                                                time__gte=time).exclude(created_by=request.user).order_by('-time')
+    #future events the user has joined
+
+
+    #past events the user has joined
+    past_events = StudyEvent.objects.filter(participants=request.user,
+                                            time__lt=time).exclude(created_by=request.user).order_by(
+        '-time') #order by most recent event first
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -43,6 +51,8 @@ def profile_view(request):
         'profile': profile,
         'user': request.user,
         'created_events': created_events,
-        'joined_events': joined_events,
+        'upcoming_events': upcoming_events,
+        'past_events': past_events,
+        'time': time,
     }
     return render(request, 'user_profile/profile.html', context)
